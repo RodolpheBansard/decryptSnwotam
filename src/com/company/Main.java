e2b2b0900e8099f77d3d97fa813a6c08e2bbabc7
DESKTOP-C6K58N9\Rodolphe <rodolphebansard@yahoo.com>
2019-11-12 12:09:03 +0100

    B) date et heure

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -27,11 +27,13 @@ public class Main {
             Map.Entry pair = (Map.Entry) it.next();
             switch(pair.getKey().toString().charAt(0)){
                 case 'A':
-                    System.out.print("Airport name : ");
-                    System.out.println(getName(pair.getValue().toString().replace(" ","")));
+
+                    String airportName = getAirportName(pair.getValue().toString().replace(" ",""));
+                    System.out.println(airportName);
                     break;
                 case 'B':
-
+                    String dateHour = getDateHour(pair.getValue().toString().replace(" ",""));
+                    System.out.println(dateHour);
                     break;
                 case 'C':
 
@@ -116,8 +118,7 @@ public class Main {
     }
 
 
-
-    public static String getName(String OACI) throws IOException {
+    public static String getAirportName(String OACI) throws IOException {
         File file = new File("D:\\Repos\\5A\\Android\\decryptSnowtam\\src\\com\\company\\OACI codes");
 
         BufferedReader br = new BufferedReader(new FileReader(file));
@@ -135,4 +136,58 @@ public class Main {
         }
         return "can't find airport";
     }
+
+
+    public static String getDateHour(String data){
+        String day = data.substring(2,4);
+        String month = "";
+        String heure = data.substring(4,6) + "h" + data.substring(6,8) + " UTC";
+
+        switch(data.substring(0,2))
+        {
+            case "01":
+                month = "January";
+                break;
+            case "02":
+                month = "February";
+                break;
+            case "03":
+                month = "March";
+                break;
+            case "04":
+                month = "April";
+                break;
+            case "05":
+                month = "May";
+                break;
+            case "06":
+                month = "June";
+                break;
+            case "07":
+                month = "July";
+                break;
+            case "08":
+                month = "August";
+                break;
+            case "09":
+                month = "September";
+                break;
+            case "10":
+                month = "October";
+                break;
+            case "11":
+                month = "November";
+                break;
+            case "12":
+                month = "December";
+                break;
+            default:
+                month = "error";
+        }
+
+        return day + " " + month + " " + heure;
+    }
+
+
+
 }
